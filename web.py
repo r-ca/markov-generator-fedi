82c7717876ba9a1a2d87d13ff4ad4b7f1c995403
rca <_@r0m.me>
2025-07-03 16:43:33 +0900

    Revert "change listen address(for debug)"
    This reverts commit 1517c04ce85bba8ce71512a751f55190cdeb1b3d.

--- web.py
+++ web.py
@@ -730,4 +730,4 @@ except AttributeError:
     pass
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=PORT, debug=DEBUG, threaded=True)
+    app.run(host='127.0.0.1', port=PORT, debug=DEBUG, threaded=True)
